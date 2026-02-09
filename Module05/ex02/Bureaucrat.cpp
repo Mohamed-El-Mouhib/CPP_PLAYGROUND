a855ddd49d45eac343f983ce27a8a768b1bfe6bf
Mohamed-El-Mouhib <huntermed15@gmail.com>
2026-02-09 12:03:07 +0100

    bureaucrat can now execute form

---
+++ Module05/ex02/Bureaucrat.cpp
@@ -0,0 +1,97 @@
+#include "Bureaucrat.hpp"
+
+static Grade GradeValidate( Grade grade )
+{
+   if ( grade > 150 )
+      throw Bureaucrat::GradeTooLowException();
+   else if ( grade < 1 )
+      throw Bureaucrat::GradeTooHighException();
+   return grade;
+}
+
+Bureaucrat::Bureaucrat( void ) : _name( "Bureaucrat' default" ), _grade( 150 )
+{
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat &other )
+  : _name( other._name ), _grade( GradeValidate( other._grade ) )
+{
+}
+
+Bureaucrat::Bureaucrat( const std::string &name, Grade grade )
+  : _name( name ), _grade( GradeValidate( grade ) )
+{
+}
+
+Bureaucrat &Bureaucrat::operator=( const Bureaucrat &other )
+{
+   if ( this != &other )
+   {
+      _name  = other._name;
+      _grade = other._grade;
+   }
+   return *this;
+}
+
+Bureaucrat::~Bureaucrat( void )
+{
+}
+
+void Bureaucrat::incGrade( void )
+{
+   _grade--;
+   GradeValidate( _grade );
+}
+
+void Bureaucrat::decGrade( void )
+{
+   _grade++;
+   GradeValidate( _grade );
+}
+
+void Bureaucrat::signAForm( AForm &form )
+{
+   try
+   {
+      form.beSigned( *this );
+      std::cout << _name << " signed '" << form.getName() << "' Form"
+                << std::endl;
+   }
+   catch ( std::exception &e )
+   {
+      std::cout << _name << " couldn’t sign '" << form.getName()
+                << "' Form, because " << e.what() << std::endl;
+   }
+}
+
+std::string Bureaucrat::getName( void ) const
+{
+   return _name;
+}
+
+Grade Bureaucrat::getGrade( void ) const
+{
+   return _grade;
+}
+
+void Bureaucrat::executeForm( AForm const &form )
+{
+   try
+   {
+      form.execute( *this );
+      std::cout << _name << " executed '" << form.getName() << "' form"
+                << std::endl;
+   }
+   catch ( std::exception &e )
+   {
+      std::cout << _name << " couldn't execute " << form.getName()
+                << " because " << e.what() << std::endl;
+   }
+}
+
+std::ostream &operator<<( std::ostream &out, const Bureaucrat &c )
+{
+   out << c.getName() << ", Bureaucrat grade " << c.getGrade() << "."
+       << std::endl;
+   return out;
+}
