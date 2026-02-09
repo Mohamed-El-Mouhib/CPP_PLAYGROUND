b01dfaed9b6ee13db95cf2f39676070d69b5de25
Mohamed-El-Mouhib <huntermed15@gmail.com>
2026-02-09 12:06:20 +0100

    shrubbery is now being planted inside file "target"_shrubbery

---
+++ Module05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,88 @@
+#include "ShrubberyCreationForm.hpp"
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+#include <stdexcept>
+
+static void DrawTree( std::fstream &file )
+{
+   file << "          . . .                                  \n"
+        << "        .        .  .     ..    .                \n"
+        << "     .                 .         .  .            \n"
+        << "                     .                           \n"
+        << "                    .                ..          \n"
+        << "    .          .            .              .     \n"
+        << "    .            '.,        .               .    \n"
+        << "    .              'b      *                     \n"
+        << "     .              '$    #.                ..   \n"
+        << "    .    .           $:   #:               .     \n"
+        << "  ..      .  ..      *#  @):        .   . .      \n"
+        << "               .     :@,@):   ,.**:'   .         \n"
+        << "   .      .,         :@@*: ..**'      .   .      \n"
+        << "            '#o.    .:(@'.@*''  .                \n"
+        << "    .  .       'bq,..:,@@*'   ,*      .  .       \n"
+        << "               ,p$q8,:@)'  .p*'      .           \n"
+        << "        .     '  . '@@Pp@@*'    .  .             \n"
+        << "         .  . ..    Y7'.'     .  .               \n"
+        << "                   :@):.                         \n"
+        << "                  .:@:'.                         \n";
+}
+
+static void openFile( const std::string &target, std::fstream &file )
+{
+   std::string openPath = target + "_shrubbery";
+
+   file.open( openPath.c_str(), std::fstream::trunc | std::fstream::out );
+   if ( !file )
+      throw std::runtime_error( openPath + ": File not inaccessible" );
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm( void )
+  : AForm::AForm( "ShrubberyCreationForm", 145, 137 )
+{
+   _target = "default";
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm( const std::string &target )
+  : AForm::AForm( "ShrubberyCreationForm", 145, 137 )
+{
+   _target = target;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(
+    const ShrubberyCreationForm &other )
+  : AForm::AForm( other.getName(), other.getRequiredGrade(),
+                  other.getExecuteGrade() )
+{
+   _target = other._target;
+}
+
+ShrubberyCreationForm &
+ShrubberyCreationForm::operator=( const ShrubberyCreationForm &other )
+{
+   if ( this != &other )
+   {
+      AForm::operator=( other );
+      _target = other._target;
+   }
+
+   return *this;
+}
+
+void ShrubberyCreationForm::execute( const Bureaucrat &executer ) const
+{
+   if ( !isSigned() )
+      throw AForm::NotSignedException();
+   if ( executer.getGrade() <= getExecuteGrade() )
+   {
+      std::fstream file;
+      openFile( _target, file );
+      DrawTree( file );
+      file.close();
+   }
+   else
+      throw Bureaucrat::GradeTooLowException();
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm( void )
+{
+}
