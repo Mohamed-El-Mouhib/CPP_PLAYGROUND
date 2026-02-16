ac815cd208c23be5e95dd0f786edf20119b345cf
Mohamed-El-Mouhib <huntermed15@gmail.com>
2026-02-16 19:32:32 +0000

    our template class Array

--- Module07/ex02/Array.hpp
+++ Module07/ex02/Array.hpp
@@ -16,22 +16,23 @@ template <typename A> class Array
       }
 
       Array( const Array &other )
-        : _size( other._size ),
-          _arr( operator new[]( sizeof( A ) * other.size() ) )
+        : _size( other._size ), _arr( static_cast<int *>( operator new[](
+                                    sizeof( A ) * other.size() ) ) )
       {
          for ( size_t i = 0; i < other.size(); ++i )
             _arr[ i ] = other._arr[ i ];
       }
 
       Array( unsigned int n )
-        : _size( n ), _arr( operator new( sizeof( A ) * n ) )
+        : _size( n ),
+          _arr( static_cast<int *>( operator new[]( sizeof( A ) * n ) ) )
       {
          std::memset( _arr, 0, sizeof( A ) * n );
       }
 
       ~Array( void )
       {
-         operator delete( _arr );
+         operator delete[]( _arr );
       }
 
       Array &operator=( const Array &other )
@@ -39,7 +40,7 @@ template <typename A> class Array
          if ( this != &other )
          {
             operator delete( _arr );
-            _arr = operator new( sizeof( A ) * other._size );
+            _arr = operator new[]( sizeof( A ) * other._size );
             for ( size_t i = 0; i < other.size(); ++i )
                _arr[ i ] = other._arr[ i ];
          }
@@ -47,7 +48,7 @@ template <typename A> class Array
          return *this;
       }
 
-      int &operator[]( const int i )
+      A &operator[]( const unsigned int i )
       {
          if ( i >= _size )
             throw std::out_of_range( "out of bound index" );
