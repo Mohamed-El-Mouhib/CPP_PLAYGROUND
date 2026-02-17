0b8be7d70f8dfaf5acd90ab85395adbf90057471
Mohamed El mouhib <huntermed15@gmail.com>
2026-02-17 01:08:38 +0000

    replaced class keyword with typename

--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -1,7 +1,7 @@
 #ifndef _IMPL_TEMPLATE__
 #define _IMPL_TEMPLATE__
 
-template <class T> void swap( T &First, T &Second )
+template <typename T> void swap( T &First, T &Second )
 {
    T tmp;
 
@@ -10,12 +10,12 @@ template <class T> void swap( T &First, T &Second )
    Second = tmp;
 }
 
-template <class T> T &min( T &First, T &Second )
+template <typename T> T &min( T &First, T &Second )
 {
    return Second <= First ? Second : First;
 }
 
-template <class T> T &max( T &First, T &Second )
+template <typename T> T &max( T &First, T &Second )
 {
    return Second >= First ? Second : First;
 }
