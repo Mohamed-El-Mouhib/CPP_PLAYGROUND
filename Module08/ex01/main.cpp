1a8ef04bcefc24dc5913cd87215528fb4130cca9
Mohamed-El-Mouhib <huntermed15@gmail.com>
2026-02-18 19:36:10 +0000

    removing Calling Debugger function on mainFunc

--- Module08/ex01/main.cpp
+++ Module08/ex01/main.cpp
@@ -3,18 +3,24 @@
 
 int main()
 {
-   int arr[] = { 6, 3, 17, 9, 11 };
-   Span sp   = Span( arr, arr + ( sizeof( arr ) / sizeof( int ) ) );
+   {
+      int arr[] = { 6, 3, 17, 9, 11 };
+      Span sp   = Span( arr, arr + ( sizeof( arr ) / sizeof( int ) ) );
 
-   sp.printNumbers();
-   // sp.addNumber( 6 );
-   // sp.addNumber( 3 );
-   // sp.addNumber( 17 );
-   // sp.addNumber( 9 );
-   // sp.addNumber( 11 );
+      std::cout << sp.shortestSpan() << std::endl;
+      std::cout << sp.longestSpan() << std::endl;
+   }
+   // SCOP!!!
+   {
+      Span sp = Span( 5 );
 
-   std::cout << sp.shortestSpan() << std::endl;
-   std::cout << sp.longestSpan() << std::endl;
+      sp.addNumber( 6 );
+      sp.addNumber( 3 );
+      sp.addNumber( 17 );
+      sp.addNumber( 9 );
+      sp.addNumber( 11 );
 
-   return 0;
+      std::cout << sp.shortestSpan() << std::endl;
+      std::cout << sp.longestSpan() << std::endl;
+   }
 }
